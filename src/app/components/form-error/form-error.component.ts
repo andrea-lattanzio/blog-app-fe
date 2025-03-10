7a0cc59b8d1731cfc17a1249da0a6aedd65f55b6
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-10 18:47:35 +0100

    feat(forms): add reusable error component

---
+++ src/app/components/form-error/form-error.component.ts
@@ -0,0 +1,42 @@
+import { CommonModule } from '@angular/common';
+import { Component, Input, input, OnInit } from '@angular/core';
+import { ValidationErrors } from '@angular/forms';
+
+@Component({
+  selector: 'app-form-error',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './form-error.component.html',
+  styleUrl: './form-error.component.scss',
+})
+export class FormErrorComponent implements OnInit {
+  @Input() error!: ValidationErrors | null;
+  message: string = '';
+
+  ngOnInit(): void {
+    if (this.error) {
+      const errorKey = Object.keys(this.error)[0] as keyof ValidationErrors; // Explicitly cast the error key
+      this.message = this.getErrorMessage(errorKey, this.error);
+    }
+  }
+
+  getErrorMessage(
+    errorKey: keyof ValidationErrors,
+    errors: ValidationErrors
+  ): string {
+    switch (errorKey) {
+      case 'required':
+        return 'This field is required';
+      case 'minlength':
+        return `Minimum length required is ${errors['minlength'].requiredLength}`;
+      case 'maxlength':
+        return `Maximum length allowed is ${errors['maxlength'].requiredLength}`;
+      case 'email':
+        return 'Please enter a valid email address';
+      case 'pattern':
+        return 'Password must contain at least 8 characters, including uppercase, lowercase letters, and numbers ';
+      default:
+        return 'Invalid input';
+    }
+  }
+}

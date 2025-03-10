46a6860111ffc9d0bc3b2259f7bb15b4c517f10f
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-10 23:40:25 +0100

    update(navbar): hide links on smaller screens, redirect to home when click on logo

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { NavlinksComponent } from '../navlinks/navlinks.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
   imports: [NavlinksComponent],
   templateUrl: './navbar.component.html',
-  styleUrl: './navbar.component.scss'
+  styleUrl: './navbar.component.scss',
 })
 export class NavbarComponent {
+  private readonly router = inject(Router);
 
+  navigate(route: string): void {
+    this.router.navigate([route]);
+  }
 }

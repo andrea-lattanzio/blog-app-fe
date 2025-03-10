99e5a541ca908db4c6b9d3b3b1510a7dbbcabbc4
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-10 16:36:41 +0100

    feat(login-form): add register form component and layout, (actual auth logic still missing)

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ButtonComponent, ButtonInput } from '../../components/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  styleUrl: './register.component.scss',
 })
 export class RegisterComponent {
+  private readonly router = inject(Router);
+  buttonInputs: ButtonInput = {
+    type: 'submit',
+    icon: 'bi bi-box-arrow-in-right',
+    text: 'Sign up',
+    styles: 'w-full font-semibold font-poppins',
+  };
+  handleRegister(): void {}
 
+  navigate(route: string): void {
+    this.router.navigate([route]);
+  }
 }

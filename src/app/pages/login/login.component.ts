eb85c5df7162e8f3e648284b8b10b5add34f622b
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-10 16:28:06 +0100

    feat(login-form): add login for component and layout, (actual auth logic still missing)

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ButtonComponent, ButtonInput } from '../../components/button/button.component';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   standalone: true,
-  imports: [],
+  imports: [ButtonComponent],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  private readonly router = inject(Router);
+  buttonInputs: ButtonInput = {
+    type: 'submit',
+    icon: 'bi bi-box-arrow-in-right',
+    text: 'Login',
+    styles: 'w-full font-semibold font-poppins',
+  }
 
+  handleLogin(): void {
+    console.log('sas');
+  }
+
+  navigate(route: string): void {
+    this.router.navigate([route]);
+  }
 }

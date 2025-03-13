24aa4c603cf48865f55b8a0c1f5cb5df3f9e1b32
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-13 23:59:58 +0100

    feat(homepage): add style for homepage

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,12 +1,62 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import {
+  ButtonComponent,
+  ButtonInput,
+} from '../../components/button/button.component';
+import {
+  FormGroup,
+  FormsModule,
+  NonNullableFormBuilder,
+  ReactiveFormsModule,
+  ValidationErrors,
+  Validators,
+} from '@angular/forms';
+import { FormErrorComponent } from '../../components/form-error/form-error.component';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [],
+  imports: [
+    ButtonComponent,
+    FormsModule,
+    ReactiveFormsModule,
+    FormErrorComponent,
+  ],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  private readonly fb = inject(NonNullableFormBuilder);
+  public newsletterForm!: FormGroup;
+  subscribeButtonInputs: ButtonInput = {
+    type: 'submit',
+    text: 'Subscribe',
+    styles: 'w-full font-semibold font-playfair mt-2',
+  };
 
+  ngOnInit(): void {
+    this.newsletterForm = this.fb.group(
+      {
+        email: ['', Validators.required, Validators.email],
+      },
+      { updateOn: 'submit' }
+    );
+  }
+
+  handleNewsletterSubscription(): void {
+    if(this.newsletterForm.invalid) return;
+    
+  }
+
+  hasError(control: string): boolean {
+    return (
+      this.newsletterForm.controls[control].invalid &&
+      this.newsletterForm.controls[control].touched
+    );
+  }
+
+  getError(control: string): ValidationErrors | null {
+    const errors = this.newsletterForm.controls[control].errors;
+    return errors;
+  }
 }

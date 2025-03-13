096c30f7ad83a1e8fd2f204416a3a6dac99afbce
Ltensio <76816528+Ltensio@users.noreply.github.com>
2025-03-13 17:38:19 +0100

    feat(spinner): update spinner component to display lottie animation

--- src/app/components/spinner/spinner.component.ts
+++ src/app/components/spinner/spinner.component.ts
@@ -1,12 +1,17 @@
 import { Component } from '@angular/core';
+import { AnimationOptions, LottieComponent } from 'ngx-lottie';
 
 @Component({
   selector: 'app-spinner',
   standalone: true,
-  imports: [],
+  imports: [LottieComponent],
   templateUrl: './spinner.component.html',
-  styleUrl: './spinner.component.scss'
+  styleUrl: './spinner.component.scss',
 })
 export class SpinnerComponent {
-
+  animationOptions: AnimationOptions = {
+    path: 'assets/lotties/spinner.json',
+    loop: true,
+    autoplay: true,
+  };
 }

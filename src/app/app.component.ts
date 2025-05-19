2e5836bb7ded8a71b7e3470af9412214cae2f074
Andrea Lattanzio <andrea.lattanzio@consulting4business.it>
2025-05-19 17:14:57 +0200

    feat(svg icons): add possibility of using svg icons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,13 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { DomSanitizer } from '@angular/platform-browser';
 import { LoadingService } from './services/loading.service';
 import { SpinnerComponent } from './components/spinner/spinner.component';
 import { AlertService } from './services/alert.service';
 import { Alert, AlertComponent } from './components/alert/alert.component';
 import { CommonModule } from '@angular/common';
+import { MatIconRegistry } from '@angular/material/icon';
 
 @Component({
   selector: 'app-root',
@@ -22,10 +24,18 @@ import { CommonModule } from '@angular/common';
 })
 export class AppComponent implements OnInit {
   title = 'blog-app-fe';
+  private readonly matIconRegistry = inject(MatIconRegistry);
+  private readonly domSanitizer = inject(DomSanitizer);
   private readonly loadingService = inject(LoadingService);
   private readonly alertService = inject(AlertService);
   loading: boolean = false;
   alerts$ = this.alertService.alerts$;
+
+  constructor() {
+    this.matIconRegistry.addSvgIconSet(
+      this.domSanitizer.bypassSecurityTrustResourceUrl('assets/icons/sprite.svg')
+    );
+  }
 
   ngOnInit(): void {
     this.loadingService.loading$.subscribe((value) => (this.loading = value));

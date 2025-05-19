ceca6b11667a0612a83e4e8099db839361b30cd2
Andrea Lattanzio <andrea.lattanzio@consulting4business.it>
2025-05-19 17:39:29 +0200

    react component TODO

--- src/app/pages/react/react.component.ts
+++ src/app/pages/react/react.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
+import { MatIconModule } from '@angular/material/icon';
+import { ArticleCardComponent } from '../../components/article-card/article-card.component';
 
 @Component({
   selector: 'app-react',
   standalone: true,
-  imports: [],
+  imports: [MatIconModule, ArticleCardComponent],
   templateUrl: './react.component.html',
   styleUrl: './react.component.scss'
 })

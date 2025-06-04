eb1cc5047f9a09a94f8287cc717da0721f40df3e
Andrea Lattanzio <andrea.lattanzio@consulting4business.it>
2025-06-04 15:23:45 +0200

    add code section component with dependencies

---
+++ src/app/components/code-section/code-section.component.ts
@@ -0,0 +1,26 @@
+import { Component, inject, Input, OnInit } from '@angular/core';
+import { DomSanitizer, SafeHtml } from '@angular/platform-browser';
+import { codeToHtml } from 'shiki';
+import { CodeSection } from '../../services/interfaces/article/codesection.interface';
+
+@Component({
+  selector: 'app-code-section',
+  standalone: true,
+  imports: [],
+  templateUrl: './code-section.component.html',
+  styleUrl: './code-section.component.scss',
+})
+export class CodeSectionComponent implements OnInit {
+  @Input({ required: true }) codeSection: CodeSection | null = null;
+  public safeHtml: SafeHtml = '';
+  private readonly sanitizer: DomSanitizer = inject(DomSanitizer);
+
+  async ngOnInit(): Promise<void> {
+    if(!this.codeSection) return;
+    const rawHtml = await codeToHtml(this.codeSection?.code, {
+      lang: this.codeSection?.language,
+      theme: 'one-dark-pro'
+    });
+    this.safeHtml = this.sanitizer.bypassSecurityTrustHtml(rawHtml);
+  }
+}

6c908411dbc7de8088c332ad1e599cdcd675242a
Andrea Lattanzio <andrea.lattanzio@consulting4business.it>
2025-06-23 10:47:22 +0200

    feat(ArticleComponent): initial full article component set up
    - add fullArticle$ observable in  article service
    - add subscription in article component

--- src/app/pages/article/article.component.ts
+++ src/app/pages/article/article.component.ts
@@ -1,12 +1,36 @@
-import { Component } from '@angular/core';
+import { animate, style, transition, trigger } from '@angular/animations';
+import { Component, inject, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { ArticleService } from '../../services/article.service';
+import { AuthService } from '../../services/auth.service';
+import { AsyncPipe } from '@angular/common';
 
 @Component({
   selector: 'app-article',
   standalone: true,
-  imports: [],
+  imports: [AsyncPipe],
   templateUrl: './article.component.html',
-  styleUrl: './article.component.scss'
+  styleUrl: './article.component.scss',
+  animations: [
+      trigger('fadeIn', [
+        transition(':enter, * => *', [
+          style({ opacity: 0 }),
+          animate('1200ms ease-in-out', style({ opacity: 1 })),
+        ]),
+      ]),
+    ],
 })
-export class ArticleComponent {
-  
+export class ArticleComponent implements OnInit {
+  private readonly route = inject(ActivatedRoute);
+  private readonly articleSrv = inject(ArticleService);
+  private readonly authSrv = inject(AuthService);
+
+  public article$ = this.articleSrv.fullArticle$;
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe((params) => {
+      const articleId = params.get('id')!;
+      this.articleSrv.getFullArticle(articleId);
+    });
+  }
 }
--- src/app/services/article.service.ts
+++ src/app/services/article.service.ts
@@ -24,13 +24,14 @@ export class ArticleService {
   private readonly authSrv = inject(AuthService);
   private readonly alertSrv = inject(AlertService);
 
-  private _articles$ = new BehaviorSubject<PaginatedResult<Article> | null>(
-    null
-  );
+  private _articles$ = new BehaviorSubject<PaginatedResult<Article> | null>(null);
   articles$ = this._articles$.asObservable();
 
   private _latestArticles$ = new BehaviorSubject<Article[] | null>(null);
   latestArticles$ = this._latestArticles$.asObservable();
+
+  private _fullArticle$ = new BehaviorSubject<Article | null>(null);
+  fullArticle$ = this._fullArticle$.asObservable();
 
   constructor() { }
 
@@ -67,11 +68,34 @@ export class ArticleService {
     headers = headers.set('show-spinner', 'false');
 
     this.http
-      .get<Article[]>(`${environment.uri}/article/latest-three`, {
-        headers: headers,
-      })
+      .get<Article[]>(`${environment.uri}/article/latest-three`, { headers: headers })
+      .pipe(
+        catchError((err) => {
+          this.alertSrv.error("Unable to load articles");
+          return throwError(() => err);
+        })
+      )
       .subscribe((articles: Article[]) => {
         this._latestArticles$.next(articles);
+      });
+  }
+
+  public getFullArticle(articleId: string): void {
+    this._fullArticle$.next(null);
+    let headers = new HttpHeaders();
+    headers = headers.set('show-spinner', 'false');
+
+    this.http
+      .get<Article>(`${environment.uri}/article/${articleId}`, { headers: headers })
+      .pipe(
+        catchError((err) => {
+          console.log(err);
+          this.alertSrv.error("Unable to load this article");
+          return throwError(() => err);
+        })
+      )
+      .subscribe((article: Article) => {
+        this._fullArticle$.next(article);
       });
   }
 }
